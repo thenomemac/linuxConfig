7767f20bf7b3b94eede92c8cb7b6502632319bdb
Josiah <thenomemac@gmail.com>
2016-08-13 15:17:09 -0400

    more progress on building out pc

--- ubuntuConfig.sh
+++ ubuntuConfig.sh
@@ -20,5 +20,25 @@ python -c "import numpy as np; print(np.__config__.show());"
 python -c "import numpy as np; x = np.random.random((5000, 5000)); print(x.dot(x).shape);"
 
 # git config
-git config --global user.email "thenomemac@gmail.com"thenome@lpc:~/git config --global user.name "Josiah"
+git config --global user.email "thenomemac@gmail.com"
+git config --global user.name "Josiah"
+
+# deep learning bleeding edge installs
+pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
+pip install --upgrade https://github.com/fchollet/keras/archive/master.zip
+pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+
+# test blas config
+wget https://github.com/Theano/Theano/raw/master/theano/misc/check_blas.py
+python check_blas.py
+rm check_blas.py
+
+# install python graphing tools
+pip install --upgrade seaborn ggplot altair plotly
+# install python utils
+pip install --upgrade urllib2 urllib3 tqdm
+# install ml tools 
+pip install --upgrade xgboost h2o 
+
+
 
