c6dac8e26fdf381f75087dbc6a3a034b9b27356b
Josiah Olson <thenomemac@gmail.com>
2017-09-10 08:41:16 -0400

    added bootstrap step

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+# run this script to bootstrap git config
+sudo apt-get install -y git
+git config --global user.name "Josiah Olson"
+git config --global user.email "thenomemac@gmail.com"
+git config --global credential.helper store
+
+# clone the config repo
+git clone https://github.com/linuxConfig.git
+
