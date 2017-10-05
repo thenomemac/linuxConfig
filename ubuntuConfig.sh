d89c649aca49242769777f31587d43d142006393
Josiah Olson <thenomemac@gmail.com>
2017-10-04 20:52:30 -0400

    added emacs and flake and git enhancements

--- ubuntuConfig.sh
+++ ubuntuConfig.sh
@@ -8,6 +8,11 @@ sudo apt-get install -y git curl htop tmux emacs
 
 # emacs config
 git clone https://github.com/thenomemac/.emacs.d.git
+emacs --script ~/.emacs.d/init.el
+
+# global gitignore
+git config --global core.excludesfile ~/.gitignore_global
+cp ./.gitignore_global ~/.gitignore_global
 
 # get the latest conda build and install
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
@@ -22,6 +27,10 @@ conda install -y numpy pandas scipy matplotlib mkl ipython jupyter requests flas
 # linting dev tools
 pip install jedi rope flake8 importmagic autopep8 yapf
 
+# flake config
+mkdir -p ~/.config
+echo -e '[flake8]\nmax-line-length = 120' >> ~/.config/flake8
+
 # docker install
 sudo apt-get install docker.io
 # make docker non root
@@ -35,13 +44,13 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 source ~/.bashrc
 bash-it enable alias git docker tmux vim emacs apt curl clipboard
 
+# personal bin
+mkdir -p ~/bin
+echo -e '\n# personal bin\nexport PATH="$HOME/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
 # google cloud config
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install -y google-cloud-sdk kubectl
-
-# personal bin
-mkdir -p ~/bin
-echo -e '\n# personal bin\nexport PATH="$HOME/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
