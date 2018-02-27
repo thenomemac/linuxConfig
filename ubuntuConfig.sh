265eba091a9572a573684d77a4c71887db3851e7
Josiah Olson <thenomemac@gmail.com>
2018-02-26 21:44:31 -0500

    changes to docker

--- ubuntuConfig.sh
+++ ubuntuConfig.sh
@@ -33,20 +33,20 @@ mkdir -p ~/.config
 echo -e '[flake8]\nmax-line-length = 120' >> ~/.config/flake8
 
 # docker install
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get update -y
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-commo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+sudo apt-get remove -y docker docker-engine docker.io &&
+sudo apt-get update -y &&
+# sudo apt-get install -y \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     software-properties-commo &&
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
+sudo apt-key fingerprint 0EBFCD88 &&
 sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
-    stable"
-sudo apt-get update -y
+    stable" &&
+sudo apt-get update -y &&
 sudo apt-get install -y docker-ce
 
 # make docker non root
