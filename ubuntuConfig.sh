b928d170430479af8ba4ef9954f4bf1276655809
Josiah Olson <thenomemac@gmail.com>
2018-04-15 11:06:27 -0400

    add docker-compose

--- ubuntuConfig.sh
+++ ubuntuConfig.sh
@@ -49,6 +49,11 @@ sudo add-apt-repository \
     stable" &&
 sudo apt-get update -y &&
 sudo apt-get install -y docker-ce
+# docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose &&
+sudo chmod +x /usr/local/bin/docker-compose &&
+docker-compose --version  # docker-compose version 1.21.0, build 1719ceb
+
 
 # make docker non root
 sudo groupadd docker
